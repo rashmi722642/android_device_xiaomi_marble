e2c346d02d28ea5ceac1d4d92be6b83ed06ac2e5
root <root@lsd.segfault.net>
2026-08-30 07:27:02 +0000

    marble: Adapt product config for PixelOS 17

--- custom_marble.mk
+++ custom_marble.mk
@@ -4,11 +4,18 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+TARGET_SUPPORTS_OMX_SERVICE := false
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Inherit from marble device
 $(call inherit-product, device/xiaomi/marble/device.mk)
 
 # Inherit from common lineage configuration
 $(call inherit-product, vendor/custom/config/common_full_phone.mk)
+
+TARGET_SCREEN_WIDTH := 1080
 
 PRODUCT_NAME := custom_marble
 PRODUCT_DEVICE := marble
